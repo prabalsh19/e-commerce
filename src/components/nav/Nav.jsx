d277b56ef8379a61f46d21facf8150c82af4895e
prabalsh19 <40392568+prabalsh19@users.noreply.github.com>
2023-05-18 22:19:10 +0530

    UI - Added Hamburger Nav for Mobile and Tablet

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,11 @@
 import { NavLink } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import "./Nav.css";
+import { useState } from "react";
 export default function Nav() {
+  const [showMobileNav, setShowMobileNav] = useState(false);
   return (
     <div className="nav-container">
       <nav className="nav">
@@ -22,6 +26,41 @@ export default function Nav() {
             <NavLink to="/cart">My Cart</NavLink>
             <NavLink to="/account">Account</NavLink>
           </ul>
+          <div
+            onClick={() => setShowMobileNav(!showMobileNav)}
+            className="mobile-menu-icon"
+          >
+            <MenuIcon sx={{ color: "white" }} />
+          </div>
+
+          {showMobileNav && (
+            <div className="mobile-nav-links">
+              <CloseIcon
+                className="mobile-close"
+                onClick={() => setShowMobileNav(false)}
+                sx={{ color: "white" }}
+                fontSize="large"
+              />
+              <ul>
+                <div className="mobile-search-container">
+                  <input type="text" />
+                  <button>SEARCH</button>
+                </div>
+                <NavLink onClick={() => setShowMobileNav(false)} to="/products">
+                  Products
+                </NavLink>
+                <NavLink onClick={() => setShowMobileNav(false)} to="/wishlist">
+                  Wishlist
+                </NavLink>
+                <NavLink onClick={() => setShowMobileNav(false)} to="/cart">
+                  My Cart
+                </NavLink>
+                <NavLink onClick={() => setShowMobileNav(false)} to="/account">
+                  Account
+                </NavLink>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </div>

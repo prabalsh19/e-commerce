1386a880336bba1acf66155c15e9fa30a88e6f28
prabalsh19 <40392568+prabalsh19@users.noreply.github.com>
2023-05-25 21:24:48 +0530

    UI - Added Dynamic Delivery Date

--- src/pages/OrderSuccess/OrderSuccess.jsx
+++ src/pages/OrderSuccess/OrderSuccess.jsx
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 function OrderSuccess() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+
   return (
     <div className="order-success-container">
-      <h1 style={{ color: "#FF8800" }}>
+      <h1>
         <CheckCircleIcon fontSize="large" sx={{ color: "#FF8800" }} /> Order
         Placed Successfully
       </h1>
+      <p>
+        Your order will be delivered by{" "}
+        <span>
+          {day + 3}' {month}
+        </span>
+      </p>
       <Link to="/products">Explore More</Link>
     </div>
   );
